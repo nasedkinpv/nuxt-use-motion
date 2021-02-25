e4e196d1a01c1285770a960f15718d93f2b9b1db
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-25 14:00:16 +0100

    :sparkles: (demo) update demo config from motion.config.js

--- example/app/motion.config.js
+++ example/app/motion.config.js
@@ -2,12 +2,14 @@ export default {
   directives: {
     'slide-rotate-bottom': {
       initial: {
-        y: 200,
-        opacity: 0
+        y: 400,
+        opacity: 0,
+        rotate: 90
       },
       enter: {
         y: 0,
-        opacity: 1
+        opacity: 1,
+        rotate: 0
       }
     }
   }
